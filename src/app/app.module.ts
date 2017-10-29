2ca64cc18864ff9739079b8c817111e073694fa7
Zhen Du <jenkin.du@gmail.com>
2017-10-29 01:33:28 -0400

    add camera preview function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CameraPreview} from '@ionic-native/camera-preview';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +22,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CameraPreview
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform, public cameraPreview: CameraPreview) {
+    this.platform.ready().then(() => {
+      const cameraPreviewOpts: CameraPreviewOptions = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: 'rear',
+        tapPhoto: true,
+        previewDrag: true,
+        toBack: false,
+        alpha: 1
+      };
+      console.log(window.screen.width+"x"+window.screen.height);
+      this.cameraPreview.startCamera(cameraPreviewOpts);
+      console.log('HomePage startCamera');
+      // this.cameraPreview.takePicture(function(imgData){
+      //   console.log('HomePage takePicture');
+      //   console.log(imgData);
+      // });
 
+    });
   }
 
+  startCameraAbove() {
+    console.log('Begin startCamera');
+    this.platform.ready().then(() => {
+      // const cameraPreviewOpts: CameraPreviewOptions = {
+      //   x: 0,
+      //   y: 0,
+      //   width: window.screen.width,
+      //   height: window.screen.height,
+      //   camera: 'rear',
+      //   tapPhoto: true,
+      //   previewDrag: true,
+      //   toBack: false,
+      //   alpha: 1
+      // };
+      // console.log(window.screen.width+"x"+window.screen.height);
+      // this.cameraPreview.startCamera(cameraPreviewOpts);
+      // console.log('HomePage startCamera');
+      this.cameraPreview.takePicture(function(imgData){
+        console.log('HomePage takePicture');
+        console.log(imgData);
+      });
+
+    });
+  }
 }
